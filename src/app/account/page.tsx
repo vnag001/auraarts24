f6c9acb43c3853d42b52f031a0adae791aa6b1b0
vinay n <vinaynag51@gmail.com>
2026-07-12 13:34:09 +0530

    Fix Supabase configured check

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -13,7 +13,7 @@ import { formatPrice } from "@/lib/utils";
 import { toast } from "sonner";
 
 const SUPABASE_CONFIGURED = Boolean(
-  process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 );
 
 interface OrderRow {
